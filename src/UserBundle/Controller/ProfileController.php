1769faf491cb315baa880fe60b3a51ba92b4075d
monmanesh <moniba.keymanesh@gmail.com>
2016-06-29 17:55:44 +0430

    inbox and sent added but refresh does not work correctly >>last refresh time should be checked and emails that were sent after that should be added to inbox

--- src/UserBundle/Controller/ProfileController.php
+++ src/UserBundle/Controller/ProfileController.php
@@ -337,7 +337,7 @@ class ProfileController extends BaseController
 
         }
 
-        $date = new \DateTime();
+        $date = new \DateTime() ;
 
 
         $subject = $this->required('subject');
@@ -362,6 +362,123 @@ class ProfileController extends BaseController
             )
         );
     }
+
+
+    /**
+     * @Route(path="/getInbox", name="profile_getInbox")
+     * @Template
+     * @param Request $request
+     * @return array
+     */
+
+    public function getInboxAction(Request $request)
+    {
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $userRepository = $em->getRepository("UserBundle:User");
+       // $users = $userRepository->findAll();
+        $me = $this->getUser();
+        $emails = $me->getReceivedEmails();
+
+        $output = '<?xml version="1.0" encoding="UTF-8"?>';
+        $output .= "<mails>";
+        foreach($emails as $email) {
+            $output .= "<mail>";
+            $sender = $email->getSender()->getName();
+            $output .= "<from>";
+            $output .= $sender ;
+            $output .= "</from>";
+            $receiver = $email-> getReceiver()->getName();
+            $output .= "<to>";
+            $output .= $receiver ;
+            $output .= "</to>";
+            $date = $email->getCreatedAt();
+            $content = $email -> getText();
+
+            $subject = $email->getTitle();
+            $output .= "<subject>";
+            $output .= $subject ;
+            $output .= "</subject>";
+            $output .= "<text>";
+            $output .= $content ;
+            $output .= "</text>";
+            $output .= "<date>";
+            $output .= $date->format('H:i Y/m/d '); ;
+            $output .= "</date>";
+            $output .= "<attachments>";
+            // for each attachment add an <attach></attach> tag
+            $output .= "</attachments>";
+
+            $output .= "</mail>";
+
+        }
+
+   ;
+        $output .= "</mails>";
+        header("Content-type: text/xml");
+        $xml = new \SimpleXMLElement($output);
+        echo $xml->asXML();
+        exit();
+    }
+
+    /**
+     * @Route(path="/getSent", name="profile_getSent")
+     * @Template
+     * @param Request $request
+     * @return array
+     */
+
+    public function getSentAction(Request $request)
+    {
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $userRepository = $em->getRepository("UserBundle:User");
+        // $users = $userRepository->findAll();
+        $me = $this->getUser();
+        $emails = $me->getSentEmails();
+
+        $output = '<?xml version="1.0" encoding="UTF-8"?>';
+        $output .= "<mails>";
+        foreach($emails as $email) {
+            $output .= "<mail>";
+            $sender = $email->getSender()->getName();
+            $output .= "<from>";
+            $output .= $sender ;
+            $output .= "</from>";
+            $receiver = $email-> getReceiver()->getName();
+            $output .= "<to>";
+            $output .= $receiver ;
+            $output .= "</to>";
+            $date = $email->getCreatedAt();
+            $content = $email -> getText();
+
+            $subject = $email->getTitle();
+            $output .= "<subject>";
+            $output .= $subject ;
+            $output .= "</subject>";
+            $output .= "<text>";
+            $output .= $content ;
+            $output .= "</text>";
+            $output .= "<date>";
+            $output .= $date->format('H:i Y/m/d '); ;
+            $output .= "</date>";
+            $output .= "<attachments>";
+            // for each attachment add an <attach></attach> tag
+            $output .= "</attachments>";
+
+            $output .= "</mail>";
+
+        }
+
+        ;
+        $output .= "</mails>";
+        header("Content-type: text/xml");
+        $xml = new \SimpleXMLElement($output);
+        echo $xml->asXML();
+        exit();
+    }
+
+
 
 
 
