9801acf037d659ffce7ec3ff79618375c0a8e137
monmanesh <moniba.keymanesh@gmail.com>
2016-07-01 19:21:44 +0430

    +commit

--- src/UserBundle/Controller/UserController.php
+++ src/UserBundle/Controller/UserController.php
@@ -132,7 +132,7 @@ class UserController extends BaseController
 				SecurityContextInterface::AUTHENTICATION_ERROR
 			);
 		} elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
-               $message ="invalid username or password.";
+               $message ="i";
 			$error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
 
 			$session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
